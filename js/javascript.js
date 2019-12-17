15db7b63798c83a1d3e6a801a5f112e594e8001c
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2019-12-17 16:52:51 +0100

    wip

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
 
 var nomeUtente = prompt('Qual è il tuo nome?');
 console.log(nomeUtente);
-var cognome = prompt('Qual è il tuo cognome?')
-console.log(cognome);
+var cognomeUtente = prompt('Qual è il tuo cognome?')
+console.log(cognomeUtente);
 var colore = prompt('Qual è il tuo colore preferito')
 console.log(colore);
+document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colore + '19';
