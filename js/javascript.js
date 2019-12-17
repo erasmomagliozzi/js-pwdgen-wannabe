7f833307fd75b6404ec310fb15787807b6d7f75c
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2019-12-17 16:44:23 +0100

    wip

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,7 @@
-var nome = prompt('il tuo nome');
-console.log('nome');
-var cognome = prompt('il tuo cognome')
-console.log('cognome');
+
+var nomeUtente = prompt('Qual è il tuo nome?');
+console.log(nomeUtente);
+var cognome = prompt('Qual è il tuo cognome?')
+console.log(cognome);
+var colore = prompt('Qual è il tuo colore preferito')
+console.log(colore);
